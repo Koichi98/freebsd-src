74e06c07cb563b14602af9856281d73115962774
koichiimai <kotty.0704@gmail.com>
2022-09-03 18:25:15 +0900

    fixing ioctl macro constant variables

--- sys/powerpc/linux/linux.h
+++ sys/powerpc/linux/linux.h
@@ -259,4 +259,117 @@ int	linux_ptrace_getregs_machdep(struct thread *td, pid_t pid,
 #endif /* _KERNEL */
 
 
+/*
+ * ioctl
+ *
+ * XXX comments in Linux' <asm-generic/ioctl.h> indicate these
+ * could be arch-dependant...
+ */
+#define LINUX_IOCTL
+#define LINUX_IOC_VOID		0x20000000
+#define LINUX_IOC_IN		0x40000000
+#define LINUX_IOC_OUT		0x80000000
+#define LINUX_IOC_INOUT		(LINUX_IOC_IN|LINUX_IOC_OUT)
+
+
+/*
+ * termio
+ */
+#define LINUX_CFLAGS
+#define	LINUX_TCGETS		0x7413
+#define	LINUX_TCSETSW		0x7415
+#define	LINUX_TIOCSWINSZ	0x7467
+#define	LINUX_TIOCGWINSZ	0x7468
+#define	LINUX_TIOCSPGRP		0x7476
+#define	LINUX_TIOCGPGRP		0x7477
+
+#define	LINUX_IOCTL_TERMIO_MAX	LINUX_TIOCGPGRP
+
+/* c_cc characters */
+#define LINUX_VINTR 	         0
+#define LINUX_VQUIT 	         1
+#define LINUX_VERASE 	         2
+#define LINUX_VKILL	         3
+#define LINUX_VEOF	         4
+#define LINUX_VMIN	         5
+#define LINUX_VEOL	         6
+#define LINUX_VTIME	         7
+#define LINUX_VEOL2	         8
+#define LINUX_VSWTC	         9
+#define LINUX_VWERASE 	10
+#define LINUX_VREPRINT	11
+#define LINUX_VSUSP 		12
+#define LINUX_VSTART		13
+#define LINUX_VSTOP		14
+#define LINUX_VLNEXT		15
+#define LINUX_VDISCARD	16
+
+/* c_iflag bits */
+#define LINUX_IXON	0x0200
+#define LINUX_IXOFF	0x0400
+#define LINUX_IUCLC	0x1000
+#define LINUX_IMAXBEL	0x2000
+#define LINUX_IUTF8	0x4000
+
+/* c_oflag bits */
+#define LINUX_ONLCR	0x00002
+#define LINUX_OLCUC	0x00004
+#define LINUX_NLDLY	0x00300
+#define   LINUX_NL0	0x00000
+#define   LINUX_NL1	0x00100
+#define LINUX_TABDLY	0x00c00
+#define   LINUX_TAB0	0x00000
+#define   LINUX_TAB1	0x00400
+#define   LINUX_TAB2	0x00800
+#define   LINUX_TAB3	0x00c00
+#define   LINUX_XTABS	0x00c00		/* required by POSIX to == TAB3 */
+#define LINUX_CRDLY	0x03000
+#define   LINUX_CR0	0x00000
+#define   LINUX_CR1	0x01000
+#define   LINUX_CR2	0x02000
+#define   LINUX_CR3	0x03000
+#define LINUX_FFDLY	0x04000
+#define   LINUX_FF0	0x00000
+#define   LINUX_FF1	0x04000
+#define LINUX_BSDLY	0x08000
+#define   LINUX_BS0	0x00000
+#define   LINUX_BS1	0x08000
+#define LINUX_VTDLY	0x10000
+#define   LINUX_VT0	0x00000
+#define   LINUX_VT1	0x10000
+
+/* c_cflag bit meaning */
+#define LINUX_CBAUD		0x000000ff
+#define LINUX_CBAUDEX	0x00000000
+#define LINUX_BOTHER	0x0000001f
+#define LINUX_CSIZE		0x00000300
+#define LINUX_CS5		0x00000000
+#define LINUX_CS6		0x00000100
+#define LINUX_CS7		0x00000200
+#define LINUX_CS8		0x00000300
+#define LINUX_CSTOPB	0x00000400
+#define LINUX_CREAD		0x00000800
+#define LINUX_PARENB	0x00001000
+#define LINUX_PARODD	0x00002000
+#define LINUX_HUPCL		0x00004000
+#define LINUX_CLOCAL	0x00008000
+
+/* c_lflag bits */
+#define LINUX_ISIG	0x00000080
+#define LINUX_ICANON	0x00000100
+#define LINUX_XCASE	0x00004000
+#define LINUX_ECHO	0x00000008
+#define LINUX_ECHOE	0x00000002
+#define LINUX_ECHOK	0x00000004
+#define LINUX_ECHONL	0x00000010
+#define LINUX_NOFLSH	0x80000000
+#define LINUX_TOSTOP	0x00400000
+#define LINUX_ECHOCTL	0x00000040
+#define LINUX_ECHOPRT	0x00000020
+#define LINUX_ECHOKE	0x00000001
+#define LINUX_FLUSHO	0x00800000
+#define LINUX_PENDIN	0x20000000
+#define LINUX_IEXTEN	0x00000400
+
+
 #endif /* _POWERPC_LINUX_H_ */
